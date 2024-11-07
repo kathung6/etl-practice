f58cded74b3d7bba5d1082007ae3765814a07ea9
Katarina Hung <163061565+kathung6@users.noreply.github.com>
2024-11-06 16:51:38 -0800

    Add files via upload

---
+++ etl-practice.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import glob
+import xml.etree.ElementTree as ET
+from datetime import datetime
+import warnings
+
+warnings.simplefilter(action='ignore',category=FutureWarning)
+
+log_file = "log_file.txt"
+target_file = "transformed_data.csv"
+
+# STEP 1: EXTRACTION
+def extract_from_csv(file_to_process):
+    dataframe = pd.read_csv(file_to_process)
+    return dataframe
+
+def extract_from_json(file_to_process):
+    dataframe = pd.read_json(file_to_process, lines=True)
+    return dataframe
+
+def extract_from_xml(file_to_process):
+    dataframe = pd.DataFrame(columns=["car_model","year_of_manufacture","price","fuel"])
+    tree = ET.parse(file_to_process) #parse the data using ElementTree
+    root = tree.getroot()
+    
+    for car in root:
+        car_model = car.find("car_model").text
+        year_of_manufacture = car.find("year_of_manufacture").text
+        price = float(car.find("price").text)
+        fuel = car.find("fuel").text
+        #append to pandas dataframe
+        dataframe = pd.concat([dataframe, pd.DataFrame([{"car_model":car_model,"year_of_manufacture":year_of_manufacture,"price":price,"fuel":fuel}])], ignore_index=True)
+    return dataframe
+
+def extract():
+    #create an empty dataframe to hold extracted data
+    extracted_data = pd.DataFrame(columns=['car_model','year_of_manufacture','price','fuel'])
+    
+    #process all files
+    for csvfile in glob.glob("*.csv"):
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True)
+    
+    for jsonfile in glob.glob("*.json"):
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True)
+        
+    for xmlfile in glob.glob("*.xml"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
+
+    return extracted_data
+
+# STEP 2: TRANSFORMATION
+#convert price to rounded to two decimals
+
+def transform(data):
+    data['price'] = round(data.price, 2)
+    return data
+
+# STEP 3: LOADING AND LOGGING
+def load_data(target_file, transformed_data):
+    transformed_data.to_csv(target_file)
+    
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open(log_file,"a") as f:
+        f.write(timestamp + ',' + message + '\n')
+        
+# TESTING
+# Log the initialization of the ETL process 
+log_progress("ETL Job Started") 
+ 
+# Log the beginning of the Extraction process 
+log_progress("Extract phase Started") 
+extracted_data = extract() 
+ 
+# Log the completion of the Extraction process 
+log_progress("Extract phase Ended") 
+ 
+# Log the beginning of the Transformation process 
+log_progress("Transform phase Started") 
+transformed_data = transform(extracted_data) 
+print("Transformed Data") 
+print(transformed_data) 
+ 
+# Log the completion of the Transformation process 
+log_progress("Transform phase Ended") 
+ 
+# Log the beginning of the Loading process 
+log_progress("Load phase Started") 
+load_data(target_file,transformed_data) 
+ 
+# Log the completion of the Loading process 
+log_progress("Load phase Ended") 
+ 
+# Log the completion of the ETL process 
+log_progress("ETL Job Ended") 
